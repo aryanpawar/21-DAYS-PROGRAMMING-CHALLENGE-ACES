65a3c8735f8030f38e08d9e12f321f8ab934bb25
Aryan Pawar <66082771+aryanpawar@users.noreply.github.com>
2020-10-07 21:15:18 +0530

    Learned basic data types in Kotlin.
    Learned about number data types like byte , short , int , long , float , double.
    Also leaned conversion from one type to another.
    And learned way to write big number for easy reading.

---
+++ dataTypes.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+//    Data Types In Kotlin
+
+    val byte: Byte = 8 //8-bit signed integer
+    val short: Short = 16 //16-bit signed integer
+    val int: Int = 32 //32-bit signed integer
+    val long: Long = 64 //64-bit signed integer
+
+    val float: Float = 32.00F //32-bit floating point number
+    val double: Double = 64.00 //64-bit floating point number
+
+//    Converting Byte to Double
+    println(byte.toDouble()::class)
+
+//    Number Writing Style
+    val number : Long = 1_000_000_000
+}
