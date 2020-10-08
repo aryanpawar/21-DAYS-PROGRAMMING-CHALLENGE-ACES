737204b9f3bea342f6285f4c60072b7e6b97c6cd
Aryan Pawar <66082771+aryanpawar@users.noreply.github.com>
2020-10-08 22:04:41 +0530

    Learned String and Char Data Types in Kotlin

---
+++ Day4_dataTypes2.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var fullName: String = "ARYAN"
+    println(fullName.length) // Prints length of string
+
+    println(fullName.decapitalize()) // Decapitalizes first letter od string.
+
+    val firstName = "Aryan"
+
+    val c: Char = 'A' // 16-bit Unicode Character
+
+    var rn: String = "b"
+}
